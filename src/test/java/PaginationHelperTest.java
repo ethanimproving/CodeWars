3c15dd844e482f412675db14d58e201ea2f59054
Big Eazy <ethan.miller@aa.com>
2021-07-30 17:11:08 -0500

    update pagination tests

--- src/test/java/PaginationHelperTest.java
+++ src/test/java/PaginationHelperTest.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class PaginationHelperTest {
 
-    PaginationHelper helper = new PaginationHelper(Arrays.asList("1","2","3","4","5","6","7","8","9","10","11","12"), 3);
+    PaginationHelper<Character> helper = new PaginationHelper<>(Arrays.asList('1','2','3','4','5','6','7','8','9','1','2','3'), 4);
 
     @Test
     public void itemCountTest(){
@@ -14,13 +14,13 @@ public class PaginationHelperTest {
 
     @Test
     public void pageCountTest(){
-        Assertions.assertEquals(helper.pageCount(), 4);
+        Assertions.assertEquals(helper.pageCount(), 3);
     }
 
     @Test
     public void pageCountReturnsNumberOfPages(){
         // Arrange
-        PaginationHelper helper = new PaginationHelper(Arrays.asList("1","2","3","4","5","6","7"), 3);
+        PaginationHelper<Character> helper = new PaginationHelper<>(Arrays.asList('1','2','3','4','5','6','7'), 3);
 
         // Assert
         Assertions.assertEquals(3, helper.pageCount());
@@ -33,34 +33,39 @@ public class PaginationHelperTest {
 
     @Test
     public void pageItemCountTest2(){
-        Assertions.assertEquals(3, helper.pageItemCount(3));
+        Assertions.assertEquals(4, helper.pageItemCount(2));
+    }
+
+    @Test
+    public void pageItemCountTest5(){
+        Assertions.assertEquals(4, helper.pageItemCount(0));
     }
 
     @Test
     public void pageItemCountTest3(){
-        List<String> collection = new ArrayList<>();
+        List<Character> collection = new ArrayList<>();
         for( int i = 0; i < 44; i++ )
         {
-            collection.add( "1" );
+            collection.add( '1' );
         }
-        PaginationHelper helper = new PaginationHelper(collection, 15);
+        PaginationHelper<Character> helper = new PaginationHelper<>(collection, 15);
         Assertions.assertEquals(14, helper.pageItemCount(3));
     }
 
     @Test
     public void pageItemCountTest4(){
-        List<String> collection = new ArrayList<>();
+        List<Character> collection = new ArrayList<>();
         for( int i = 0; i < 24; i++ )
         {
-            collection.add( String.valueOf(i+1) );
+            collection.add( '0' );
         }
-        PaginationHelper helper = new PaginationHelper(collection, 10);
+        PaginationHelper<Character> helper = new PaginationHelper<>(collection, 10);
         Assertions.assertEquals(4, helper.pageItemCount(3));
     }
 
     @Test
     public void pageIndexTest1(){
-        Assertions.assertEquals(2, helper.pageIndex(3));
+        Assertions.assertEquals(2, helper.pageIndex(4));
     }
 
     @Test
